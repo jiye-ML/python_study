5860ab375e3a8d6c6c4f941b5a49a48a30a32754
jiye <woxinxie1234@163.com>
2018-03-19 15:10:13 +0800

    gif

--- readme.md
+++ readme.md
@@ -18,3 +18,27 @@
 ### video
 > 摄像头视频相关操作
 
+
+### data_structure 
+> python的一些基本数据结构
+
+
+## 遇到的问题
+
+### python版本配置
+
+1. 环境变量：在path目录下加入存在 Python.exe 的目录
+```
+C:\Anaconda2
+C:\Anaconda2\script
+```
+* 如果存在多个python版本， 只需要在另一个版本中将python.exe 改为 python3.5.exe就可以进行下面操作
+```
+python -V   #  2.7
+python3.5 -V  # 3.5
+```
+* 使用 pip 安装,可以采用
+```
+python -m pip instal package
+python3.5 -m pip install package
+```
--- tool/ToolGif.py
+++ tool/ToolGif.py
@@ -1,19 +1,18 @@
-#coding: utf-9
+# coding: utf-8
 import os
 from PIL import Image
 import images2gif
 
-"""
+'''
 
-限制条件：
-    1、必须2.x才可以
-    2、目前，图片大小必须相同
-
-需要安装：
+限制条件
+    必须2.x才可以
+    目前，图片大小必须相同
+需要安装
     import Pillow
     import images2gif-Pillow
+'''
 
-"""
 
 # 图片排序
 class SortGif:
@@ -25,10 +24,17 @@ class SortGif:
             return -1
         return 1
 
-    # 按照修改时间排序
+    # 按照修改时间比较
     @staticmethod
     def compare_mtime(x, y):
         if os.stat(x).st_mtime < os.stat(y).st_mtime:
+            return -1
+        return 1
+
+    # 按照文件名称比较
+    @staticmethod
+    def compare_filename(x, y):
+        if x < y:
             return -1
         return 1
 
@@ -37,37 +43,45 @@ class SortGif:
 
 class ToolGif:
 
-    def __init__(self, source_dir, gif_name, sort=SortGif.compare_mtime, repeat=True, duration=0.2):
+    # 初始化
+    def __init__(self, source_dir, gif_name, sort=SortGif.compare_filename, repeat=True, duration=0.2):
         self.__source_dir = source_dir
         self.__gif_name = gif_name
-        self.__sort = sort
         self.__repeat = repeat
         self.__duration = duration
+        self.__sort = sort
 
-        self.image_to_gif()
+        # 生成Gif
+        self.__images_to_gif()
 
         pass
 
-    def image_to_gif(self):
-        full_image_files = []
-        img_files = os.listdir(self.__source_dir)
-        for key in img_files:
-            full_image_files.append(self.__source_dir + key)
+    # 生成Gif
+    def __images_to_gif(self):
+        # 获取需要放到gif中的文件
+        full_img_files = []
+        _img_files = os.listdir(self.__source_dir)
+        for key in _img_files:
+            full_img_files.append(self.__source_dir + key)
 
-        full_image_files.sort(self.__sort)
+        # 按照排序规则进行排序
+        full_img_files.sort(self.__sort)
 
+        # 读取图片
         imgs = []
-        for img_file in full_image_files:
-            imgs.append(Image.open(img_file))
+        for img_file in full_img_files:
+            imgs.append(Image.open(img_file).convert("RGB"))
 
+        # 写入Gif
         images2gif.writeGif(self.__gif_name, imgs, repeat=self.__repeat, duration=self.__duration)
-    pass
+
+        pass
 
 
 if __name__ == "__main__":
 
-    img_dir = "test/img/"
-    gif_dir = "test/gif/test.gif"
+    img_dir = "terra_noerdlin_7/"
+    gif_dir =  "{}.gif".format(os.path.split(img_dir)[-2])
 
     ToolGif(img_dir, gif_dir, sort=SortGif.compare_mtime, duration=0.2)
 
