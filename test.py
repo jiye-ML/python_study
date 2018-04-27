42f76335e5d84dff7bd4b094dbe5cd6de7c72253
jiye <woxinxie1234@163.com>
2018-04-27 10:38:33 +0800

    重载运算符

--- test.py
+++ test.py
@@ -1,1 +1,1 @@
-from random import randrange@Tombola.registerclass TombolaList(list):    def pick(self):        if self:            position = randrange(len(self))            return self.pop(position)        else:            raise LookupError('pop from empty Tombolist')    load = list.extend    def loaded(self):        return bool(self)    def inspect(self):        return tuple(sorted(self))
+def vowel(c):    return c.lower() in 'aeiou'print(list(filter(vowel, "Aardvark")))
